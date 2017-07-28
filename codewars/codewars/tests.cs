eded99661a72df605877f2503040fef9b979a37c
Andy Holub <aholub@dts-doc.com>
2017-07-28 16:28:05 -0400

    another kata

--- codewars/codewars/tests.cs
+++ codewars/codewars/tests.cs
@@ -16,6 +16,14 @@ namespace codewars
         }
 
         [Test]
+        public void DiceRollingKataTest()
+        {
+            Assert.AreEqual(0, Program.Score(new int[] { 2, 3, 4, 6, 2 }), "Should be 0 :-(");
+            Assert.AreEqual(400, Program.Score(new int[] { 4, 4, 4, 3, 3 }), "Should be 400");
+            Assert.AreEqual(450, Program.Score(new int[] { 2, 4, 4, 5, 4 }), "Should be 450");
+        }
+
+        [Test]
         public void nbMonthsKataTest()
         {
             //yeah this one doesn't actually pass. Gonna need some more work, or some coffee. 
